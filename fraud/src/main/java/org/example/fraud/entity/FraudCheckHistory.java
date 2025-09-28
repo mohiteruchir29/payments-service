ac39db758a39cdccf800f42b92190011ecc334ae
Ruchir Mohite <mohiteruchir29@gmail.com>
2025-09-28 20:43:45 +0530

    Add Fraud check module and its model and MVC changes

---
+++ fraud/src/main/java/org/example/fraud/entity/FraudCheckHistory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) YEAR Your Company
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.fraud.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FraudCheckHistory {
+
+  @Id
+  @SequenceGenerator(name = "fraud_id_sequence", sequenceName = "fraud_id_sequence")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fraud_id_sequence")
+  private Integer id;
+
+  private String customerId;
+
+  private boolean fraudster;
+
+  private LocalDateTime createdAt;
+}
