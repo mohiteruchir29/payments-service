2e527b2610ad6c503b15cca5f73b3c9b5799a734
Ruchir Mohite <mohiteruchir29@gmail.com>
2025-10-05 13:16:32 +0530

    Add message producer in amqp module

---
+++ advanced-message-queue/src/main/java/org/example/amqp/config/RabbitMQMessageProducer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) YEAR Your Company
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.amqp.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class RabbitMQMessageProducer {
+
+  private final AmqpTemplate amqpTemplate;
+
+  public void publish(Object payload, String exchange, String routingKey) {
+    log.info(
+        "Publishing to exchange: {} using routing key: {} and payload: {}",
+        exchange,
+        routingKey,
+        payload);
+    amqpTemplate.convertAndSend(exchange, routingKey, payload);
+    log.info(
+        "Published to exchange: {} using routing key: {} and payload: {}",
+        exchange,
+        routingKey,
+        payload);
+  }
+}
