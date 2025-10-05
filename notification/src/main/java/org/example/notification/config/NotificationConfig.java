192840485853ad5c288a9da66595301ecb69d40a
Ruchir Mohite <mohiteruchir29@gmail.com>
2025-10-05 13:16:57 +0530

    Spotless Apply

--- notification/src/main/java/org/example/notification/config/NotificationConfig.java
+++ notification/src/main/java/org/example/notification/config/NotificationConfig.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) YEAR Your Company
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.example.notification.config;
 
 import lombok.Getter;
@@ -13,28 +27,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NotificationConfig {
 
-    @Value("${rabbitmq.exchanges.internal}")
-    private String internalExchange;
+  @Value("${rabbitmq.exchanges.internal}")
+  private String internalExchange;
 
-    @Value("${rabbitmq.queues.notification}")
-    private String notificationQueue;
+  @Value("${rabbitmq.queues.notification}")
+  private String notificationQueue;
 
-    @Value("${rabbitmq.routing-keys.internal-notification}")
-    private String internalNotificationRoutingKey;
+  @Value("${rabbitmq.routing-keys.internal-notification}")
+  private String internalNotificationRoutingKey;
 
-    @Bean
-    public TopicExchange internalTopicExchange() {
-        return new TopicExchange(this.internalExchange);
-    }
+  @Bean
+  public TopicExchange internalTopicExchange() {
+    return new TopicExchange(this.internalExchange);
+  }
 
-    @Bean
-    public Queue notificationQueue() {
-        return new Queue(this.notificationQueue);
-    }
+  @Bean
+  public Queue notificationQueue() {
+    return new Queue(this.notificationQueue);
+  }
 
-    @Bean
-    public Binding internalNotificationBinding() {
-        return BindingBuilder.bind(notificationQueue()).to(internalTopicExchange()).with(internalNotificationRoutingKey);
-    }
-
+  @Bean
+  public Binding internalNotificationBinding() {
+    return BindingBuilder.bind(notificationQueue())
+        .to(internalTopicExchange())
+        .with(internalNotificationRoutingKey);
+  }
 }
