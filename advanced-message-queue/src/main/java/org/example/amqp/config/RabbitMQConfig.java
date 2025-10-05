70f84c849ef2f41fa05fb69ff4212d355a3a061e
Ruchir Mohite <mohiteruchir29@gmail.com>
2025-10-05 12:53:04 +0530

    Spotless apply

--- advanced-message-queue/src/main/java/org/example/amqp/config/RabbitMQConfig.java
+++ advanced-message-queue/src/main/java/org/example/amqp/config/RabbitMQConfig.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) YEAR Your Company
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.example.amqp.config;
 
 import lombok.AllArgsConstructor;
@@ -13,27 +27,25 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 public class RabbitMQConfig {
 
-    private final ConnectionFactory connectionFactory;
+  private final ConnectionFactory connectionFactory;
 
-    @Bean
-    public AmqpTemplate amqpTemplate() {
-        RabbitTemplate rabbitTemplate=new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return  rabbitTemplate;
-    }
+  @Bean
+  public AmqpTemplate amqpTemplate() {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(messageConverter());
+    return rabbitTemplate;
+  }
 
-    @Bean
-    private Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+  @Bean
+  private Jackson2JsonMessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
 
-    @Bean
-    SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(messageConverter());
-        return factory;
-    }
-
-
+  @Bean
+  SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setMessageConverter(messageConverter());
+    return factory;
+  }
 }
